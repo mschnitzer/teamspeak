9caa1d5f5be5cb7eccda866306bc7d2b7e77882a
Manuel Schnitzer <webmaster@mschnitzer.de>
2018-03-04 17:45:30 +0100

    Allow access to `ip_address` and `query_port` in exception

--- lib/exceptions/server_connection_failed.rb
+++ lib/exceptions/server_connection_failed.rb
@@ -1,6 +1,9 @@
 module TeamSpeak3
   module Exceptions
     class ServerConnectionFailed < TeamSpeak3::Exceptions::StandardException
+      attr_reader :ip_address
+      attr_reader :query_port
+
       def initialize(ip_address, query_port, message)
         @ip_address = ip_address
         @query_port = query_port
